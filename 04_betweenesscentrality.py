73e123064fb43b638473ca29a4f8f8ba1ba1a693
Andreas Paul Zischg <andreas.zischg@giub.unibe.ch>
2020-12-02 17:40:41 +0100

    Update 04_betweenesscentrality.py

--- 04_betweenesscentrality.py
+++ 04_betweenesscentrality.py
@@ -15,7 +15,6 @@ edgesfile=myworkspace+"/zh_roads.shp"
 #input data: the roads file
 nodesgdf = gpd.read_file(nodesfile)
 edgesgdf = gpd.read_file(edgesfile)
-outgraphfile=myworkspace+"/graph_normalcondition.gml"
 
 #output data: the nodes distances file
 nodesbetweennesscentralityfile=open(myworkspace+"/betweennesscentrality_normalsituation.csv","w")
@@ -25,23 +24,17 @@ nodesbetweennesscentralityfile.write("nodeid"+";"+"betweennesscentrality"+"\n")
 G = nx.Graph()
 #loop through the road shapefile
 for index, row in edgesgdf.iterrows():
-    if row.ID_Road not in G:
-        length = row.SHAPE_Leng
-        nodeid1=row.nodeid1
-        nodeid2 = row.nodeid2
-        xcoord1=nodesgdf[nodesgdf["nodeid"]==row.nodeid1].x
-        ycoord1 = nodesgdf[nodesgdf["nodeid"] == row.nodeid1].y
-        if row.nodeid1 not in G:
-            G.add_node(row.nodeid1, pos=(xcoord1, ycoord1))
-        xcoord2=nodesgdf[nodesgdf["nodeid"]==row.nodeid2].x
-        ycoord2 = nodesgdf[nodesgdf["nodeid"] == row.nodeid2].y
-        if row.nodeid2 not in G:
-            G.add_node(row.nodeid2, pos=(xcoord2, ycoord2))
-        G.add_edge(row.nodeid1, row.nodeid2, weight=length)
+    length = row.SHAPE_Leng
+    nodeid1=row.nodeid1
+    nodeid2 = row.nodeid2
+    xcoord1=nodesgdf[nodesgdf["nodeid"]==row.nodeid1].x
+    ycoord1 = nodesgdf[nodesgdf["nodeid"] == row.nodeid1].y
+    G.add_node(row.nodeid1, pos=(xcoord1, ycoord1))
+    xcoord2=nodesgdf[nodesgdf["nodeid"]==row.nodeid2].x
+    ycoord2 = nodesgdf[nodesgdf["nodeid"] == row.nodeid2].y
+    G.add_node(row.nodeid2, pos=(xcoord2, ycoord2))
+    G.add_edge(row.nodeid1, row.nodeid2, weight=length)
 print("network graph created ...")
-
-#write graph to file
-nx.write_gml(G, outgraphfile)
 
 #calculate betweenness centrality for all nodes and write it to the output file
 #Betweenness centrality of a node v is the sum of the fraction of all-pairs shortest paths that pass through v.
